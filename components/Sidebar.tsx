c372b0d6fc402177cfa6e722117513c07bdd9bf9
Mahesh <mahesh@panicle.tech>
2022-05-09 10:26:35 +0530

    some issue

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,7 +6,9 @@ import { useTheme } from 'next-themes'
 
 function Sidebar() {
     const { theme, setTheme } = useTheme();
+    const [mode, setmode] = React.useState("Light")
     const changeTheme = () => {
+        mode === "Light"?setmode("Dark"):setmode("Light")
         setTheme(theme === "light" ? "dark" : "light")
     }
     return (
@@ -46,7 +48,7 @@ function Sidebar() {
 
             <button className='w-1/2 px-3 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none' onClick={() => window.open('mailto:maheshmestri73@gmail.com')}>Email me</button>
 
-            <button className='w-1/2 px-3 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400' onClick={() => {changeTheme()}}>Light UI</button>
+            <button className='w-1/2 px-3 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400' onClick={() => {changeTheme()}}>{mode} UI</button>
 
         </div>
     )
